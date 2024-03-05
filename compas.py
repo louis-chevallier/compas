6943a966f249fd5a1f2357c547653fcb1071f7ea
louis <louis.chevallier@gmail.com>
2024-03-05 15:58:21 +0100

    v1

--- compas.py
+++ compas.py
@@ -5,8 +5,10 @@ from utillc import *
 import sympy.abc as X
 from sympy import Point2D, symbols, solve, cos, sin, lambdify
 import os, sys
+import random
 
-experiment = True
+EKO()
+experiment = False
 
 if experiment :
 
@@ -43,19 +45,21 @@ a, r, ra, rb = symbols("a r ra rb")
 def S(n="") :
     s = symbols("%s_%d" % (n, l_symbols.pop(0)))
     return s
-def P(n="") :
+
+def Pf(n="") :
     x = symbols("x%s_%d" % (n, l_symbols.pop(0)))
     y = symbols("y%s_%d" % (n, l_symbols.pop(0)))
     return Point2D(x, y)
 
 
-A = P("A")
-B = P("B")
+A = Pf("A")
+B = Pf("B")
 xp = A.x + ra * cos(a)
 yp = A.y + ra * sin(a)
-P, Q = Point2D(xp, yp), P("Q")
+P, Q = Point2D(xp, yp), Pf("Q")
 
 eq = [ P.distance(Q) - r, P.distance(A) - ra, Q.distance(B) - rb]
+EKO()
 sol = solve(eq, list(P.coordinates) + list(Q.coordinates))
 EKOX(sol)
 
@@ -74,6 +78,22 @@ optimize distances wrt objectives
 
 """
 
+alpha = S("alpha")
+
+fixed = [ Pf("A"), Pf("B")]
+mobiles = []
+scalars = []
+bras = []
+eee = []
+
+def choose(l) :
+    i = random.randint(0, len(l)-1)
+    return l[i] 
+
+def brasf(P1, P2, d) :
+    eq = [ P1.distance(P2) - d]
+    return { "eq" : eq }
+
 
 def strat1() :
     """
@@ -90,7 +110,67 @@ def strat1() :
            d1, d2, d3 : scalaires
            b1, b2, b3 : bras
     """
-    return None
+    
+    Pf1, Pf2 = choose(fixed), choose(fixed)
+    Pm2 = Pf("p2")
+    d1, d2, d3 = S("d1"), S("d2"), S("d3")
+
+
+    xp = Pf1.x + d1 * cos(a)
+    yp = Pf1.y + d1 * sin(a)
+    Pm1 = Point2D(xp, yp)
+    
+    b1 = brasf(Pf1, Pm1, d1)
+    b2 = brasf(Pf2, Pm2, d2)
+    b3 = brasf(Pm1, Pm2, d3)
+    
+    return { "mobiles" : [ Pm1, Pm2], "fixed" : [Pf1, Pf2], "scalars" : [ d1, d2, d3], "bras" : [ b1, b2, b3]}
+
+def strat4() :
+    """
+    pick fix point pf, mobile point pm1
+    b1 = bras(pf, pm, d1)
+    b2 = bras(pm1, pm, d2)
+    yield :
+         pm, d1, d2, b1, b2
+    """
+    Pf1 = choose(fixed)
+    Pm1 = choose(mobiles)
+    d1, d2 = S("d1"), S("d2")
+    Pm = Pf("pm")
+    b1 = brasf(Pf1, Pm, d1)
+    b2 = brasf(Pm1, Pm, d2)
+    
+    return { "mobiles" : [ Pm ], "fixed" : [], "scalars" : [ d1, d2], "bras" : [ b1, b2]}
+    
+
+def add(strt) :
+    global fixed, mobiles, scalars, bras
+    d = strt()
+    fixed += d["fixed"]
+    mobiles += d["mobiles"]
+    scalars += d["scalars"]
+    bras += d["bras"]
+    
+    eqs = [ ee for e in d["bras"] for ee in e["eq"] ]
+    EKOX(eqs)
+    EKOX(d["mobiles"])
+    s = solve(eqs, [ c for p in d["mobiles"] for c in p.coordinates] )
+    eee.append(s)
+
+add(strat1)
+add(strat4)
+
+EKOX(bras)
+EKOX(mobiles)
+eqs = [ ee for e in bras for ee in e["eq"] ]
+EKOX(eqs)
+EKOX(mobiles)
+#s = solve(eqs, [ c for p in mobiles for c in p.coordinates] )
+#EKOX(s)
+
+
+
 
 def strat2() :
     """
@@ -109,14 +189,6 @@ def strat3() :
            pm = u A + v B
     """
 
-def strat4() :
-    """
-    pick fix point pf, mobile point pm1
-    b1 = bras(pf, pm, d1)
-    b2 = bras(pm1, pm, d2)
-    yield :
-         pm, d1, d2, b1, b2
-    """
 
 
 
